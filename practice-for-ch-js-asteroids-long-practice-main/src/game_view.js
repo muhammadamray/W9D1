325b12a25422a86925ea176af78642382a094d65
Naim Miah <mmiah@fordham.edu>
2023-08-21 14:52:30 -0400

    added object exmaple

--- practice-for-ch-js-asteroids-long-practice-main/src/game_view.js
+++ practice-for-ch-js-asteroids-long-practice-main/src/game_view.js
@@ -3,9 +3,22 @@
 // Installs key listeners to move the ship and fire bullets.
 // Installs a timer to call Game.prototype.step.
 import Game from "./game.js";
+import MovingObject from "./moving_object.js";
 class GameView {
   constructor(ctx) {}
 }
-console.log("Hello World!");
 
-export default GameView;
+const canvasEl = document.getElementById("game-canvas");
+// canvasEl.width = Game.DIM_X;
+// canvasEl.height = Game.DIM_Y;
+const ctx = canvasEl.getContext("2d");
+console.log("Webpack is working!");
+
+const mo = new MovingObject({
+  pos: [30, 30],
+  vel: [10, 10],
+  radius: 5,
+  color: "red",
+});
+
+mo.draw(ctx);
--- practice-for-ch-js-asteroids-long-practice-main/src/moving_object.js
+++ practice-for-ch-js-asteroids-long-practice-main/src/moving_object.js
@@ -8,6 +8,13 @@ class MovingObject {
     this.radius = options.radius;
     this.color = options.color;
   }
+
+  draw(ctx) {
+    ctx.fillStyle = this.color;
+    ctx.beginPath();
+    ctx.arc(this.pos[0], this.pos[1], this.radius, 0, Math.PI * 2, true);
+    ctx.fill();
+  }
 }
 
 export default MovingObject;
